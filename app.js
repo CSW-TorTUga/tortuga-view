034ca47eff722a145ef7fa22260dc7d38ef79748
Leon Strauss <LionC@Leons-MBP.fritz.box>
2015-10-30 15:37:49 +0100

    mockup stuff

---
+++ app.js
@@ -0,0 +1,36 @@
+(function() {
+
+  var PRIMARY = 'blue-grey';
+  var ACCENT = 'deep-orange';
+
+  function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+
+  angular.module('rms', ['ngMaterial'])
+    .config(['$mdThemingProvider', function($mdThemingProvider) {
+      $mdThemingProvider.theme('default')
+        .primaryPalette(PRIMARY)
+        .accentPalette(ACCENT);
+    }])
+    .run(['$rootScope', '$mdColorPalette', function($rootScope, $mdColorPalette) {
+      var primaryPalette = $mdColorPalette[PRIMARY];
+
+      var primaryColor = rgbToHex(
+        primaryPalette['500'].value[0],
+        primaryPalette['500'].value[1],
+        primaryPalette['500'].value[2]);
+
+      $rootScope.primaryColor = primaryColor;
+
+      var accentPalette = $mdColorPalette[ACCENT];
+
+      var accentColor = rgbToHex(
+        accentPalette['A100'].value[0],
+        accentPalette['A100'].value[1],
+        accentPalette['A100'].value[2]);
+
+      $rootScope.accentColor = accentColor;
+    }]);
+
+})();
