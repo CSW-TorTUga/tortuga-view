625606475c12e5227fc85e65db19f0945be08ca4
Hannes <hannes@guedelhoefer.de>
2015-11-12 13:01:25 +0100

    14396 Benutzervewaltung - added UserService

--- src/management/users/UserListController.js
+++ src/management/users/UserListController.js
@@ -1,51 +1,21 @@
-(function() {
+(function () {
 
-  angular.module('management.users')
-  .controller('UserListController', [
-    UserListController
-  ]);
+    angular.module('management.users')
+        .controller('UserListController', ['UserService',
+            UserListController
+        ]);
 
-  function UserListController() {
-    var self = this;
+    function UserListController(UserService) {
+        var self = this;
 
-    var users = [
-      {
-        firstName: 'Leon',
-        lastName: 'Strauss',
-        role: 'Dozent',
-        userName: 'LionC',
-        expires: 1447259704480
-      },
-      {
-        firstName: 'Mischa',
-        lastName: 'Holz',
-        role: 'Admin',
-        userName: 'Fohl',
-        expires: 1447267704480
-      },
-      {
-        firstName: 'Hannes',
-        lastName: 'Güdelhöfer',
-        role: 'CSW-Team',
-        userName: 'reckter',
-        expires: 1447257704480
-      },
-      {
-        firstName: 'Dennis',
-        lastName: 'Schirmer',
-        role: 'Benutzer',
-        userName: 'LordCola',
-        expires: 1447257704480
-      }
-    ];
+        var users = UserService.get();
 
-    users = users.concat(users).concat(users).concat(users);
+        self.getUsers = getUsers;
 
-    self.getUsers = getUsers;
-
-    function getUsers() {
-      return users;
+        //public
+        function getUsers() {
+            return users;
+        }
     }
-  }
 
 })();
