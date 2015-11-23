90ad9579ceb1df0fc2c759f3e7e4b7e3801a4703
Leon Strauss <leon@ilu.st>
2015-11-23 22:15:03 +0100

    Implemented AuthenticationService::login

--- src/services/AuthenticationService.js
+++ src/services/AuthenticationService.js
@@ -3,22 +3,39 @@
   angular.module('rms')
   .service('AuthenticationService', [
     '$timeout',
+    '$http',
+    'apiAddress',
     UserService
   ]);
 
-  function UserService($timeout) {
+  function UserService($timeout, $http, apiAddress) {
     var self = this;
 
     var loggedIn = false;
 
     $timeout(function() {
       loggedIn = true;
-    }, 3000)
+    }, 3000);
 
+    self.login = login;
     self.isLoggedIn = isLoggedIn;
 
     function isLoggedIn() {
       return loggedIn;
+    }
+
+    function login(username, password) {
+      $http.post(apiAddress + 'login', {
+        loginName: username,
+        password: password
+      }).then(function(response) {
+        loggedIn = true;
+      }).catch(function(response) {
+        if(response.status == 401)
+          console.warn('Wrong username and/or password'); //TODO: Impelement error toasts
+        else
+          console.warn(response);
+      });
     }
   }
 
