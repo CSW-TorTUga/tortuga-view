3e6e9ab63564dd9a00f4d211cb4a3cd5a1a452df
Leon Strauss <leon@ilu.st>
2015-11-28 13:42:56 +0100

    Implemented sessionTimeoutInterceptor

---
+++ src/interceptors/sessionTimeoutInterceptor.js
@@ -0,0 +1,47 @@
+(function() {
+
+    angular.module('rms')
+        .factory('sessionTimeoutInterceptor', [
+            '$q',
+            'errorToasts',
+            '$injector',
+            sessionTimeoutInterceptor]
+        );
+
+    function sessionTimeoutInterceptor($q, ErrorToasts, $injector) {
+        var $state = $injector('$state');
+
+        /*function onRequest(reqConfig) {
+            LoadingStateService.startedLoading();
+
+            return reqConfig;
+        }
+
+        function onResponse(response) {
+            LoadingStateService.finishedLoading();
+
+            return response;
+        }*/
+
+        function onResponseError(rejection) {
+            //LoadingStateService.finishedLoading();
+
+            if(rejection.status == 401 && $state.name == 'login') {
+                $state.go('login');
+            }
+
+            /*if(rejection.status == 502) {
+                ErrorToasts.show('Der Server ist nicht mehr erreichbar. Bitter überprüfen Sie Ihre Internetverbindung.', 'OK', 'error');
+            }*/
+
+            return $q.reject(rejection);
+        }
+
+        return {
+            /*request: onRequest,
+            response: onResponse,*/
+            responseError: onResponseError
+        };
+    }
+
+})();
