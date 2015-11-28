e252091bd34a7abfc5b4f2159bde9bb1e2d32077
Leon Strauss <leon@ilu.st>
2015-11-28 19:13:47 +0100

    Just deleted some comments

--- src/interceptors/sessionTimeoutInterceptor.js
+++ src/interceptors/sessionTimeoutInterceptor.js
@@ -3,43 +3,22 @@
     angular.module('rms')
         .factory('sessionTimeoutInterceptor', [
             '$q',
-            'ErrorToasts',
             '$injector',
-            sessionTimeoutInterceptor]
-        );
+            sessionTimeoutInterceptor
+        ]);
 
-    function sessionTimeoutInterceptor($q, ErrorToasts, $injector) {
-        var $state = $injector('$state');
-
-        /*function onRequest(reqConfig) {
-            LoadingStateService.startedLoading();
-
-            return reqConfig;
-        }
-
-        function onResponse(response) {
-            LoadingStateService.finishedLoading();
-
-            return response;
-        }*/
-
+    function sessionTimeoutInterceptor($q, $injector) {
         function onResponseError(rejection) {
-            //LoadingStateService.finishedLoading();
+            var $state = $injector.get('$state');
 
             if(rejection.status == 401 && $state.name == 'login') {
                 $state.go('login');
             }
 
-            /*if(rejection.status == 502) {
-                ErrorToasts.show('Der Server ist nicht mehr erreichbar. Bitter überprüfen Sie Ihre Internetverbindung.', 'OK', 'error');
-            }*/
-
             return $q.reject(rejection);
         }
 
         return {
-            /*request: onRequest,
-            response: onResponse,*/
             responseError: onResponseError
         };
     }
