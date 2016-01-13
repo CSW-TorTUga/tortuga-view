41b02934589cd6d474203dff026a719d8193ace9
Schir <F@u>
2016-01-13 14:51:36 +0100

    Device reservations will now be displayed and the borrowing button works

---
+++ src/deviceReservations/deviceReservations.js
@@ -0,0 +1,14 @@
+(function() {
+
+  angular.module('deviceReservations', [
+    'ui.router'
+  ]).config(['$stateProvider', function($stateProvider) {
+    $stateProvider.state('deviceReservations', {
+      url: '/deviceReservations',
+      templateUrl: 'src/deviceReservations/list.html',
+      controller: 'DeviceListController',
+      controllerAs: 'deviceReservationController'
+    });
+  }]);
+
+})();
--- src/directives/deviceReservation/DeviceReservationController.js
+++ src/directives/deviceReservation/DeviceReservationController.js
@@ -23,16 +23,36 @@
 
         //public
         function borrow() {
-            alert(self.reservation.device.name + ' ausgeliehent');
+            self.reservation.borrowed = true;
+            self.reservation = DeviceReservation.update({id: self.reservation.id},self.reservation);
         }
 
         //public
         function returnDevice() {
-            alert(self.reservation.device.name + ' zurückgegeben');
+            self.reservation.borrowed = false;
+            self.reservation = DeviceReservation.update({id: self.reservation.id},self.reservation);
         }
+
+
 
         //public
         function deleteReservation() {
+
+            var dialog = $mdDialog.confirm()
+                .title("Reservierung für  " + self.reservation.device.name + " am " +self.reservation.timeSpan.beginning
+                    + " löschen?")
+                .textContent("Den Benutzer " + user.loginname + " wirklich löschen? Dies kann nicht rückgängig gemacht werden!")
+                .ok("löschen")
+                .targetEvent(event)
+                .cancel("abbrechen");
+            $mdDialog.show(dialog).then(function() {
+                return UserService.delete({userId: user.id}).$promise;
+            }).then(function(response) {
+                self.users.splice(index, 1);
+            }).catch(function(fail) {
+                console.warn(fail);
+            });
+
             DeviceReservation.delete({id: self.reservation.id}).$promise
                 .then(function() {
                     $scope.$eval(self.onDelete);
@@ -59,8 +79,9 @@
         //public
         function isActive() {
             var now = (new Date()).getTime();
+            console.dir(self.reservation);
 
-            return now >= self.reservation.timestamp.beginning && now <= self.reservation.timestamp.end;
+            return now >= self.reservation.timeSpan.beginning && now <= self.reservation.timeSpan.end;
         }
 
         //public
