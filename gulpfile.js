81267a7f8aaf4314212b44ea5ed62016c99feeb7
Hannes <hannes@guedelhoefer.de>
2016-01-15 16:13:18 +0100

    fixed date format

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,7 @@ var paths = {
         'src/management/management.js',
         'src/management/users/users.js',
         'src/management/roles/roles.js',
+        'src/management/admin/admin.js',
         'src/home/home.js',
         'src/management/booking/booking.js',
         'src/login/login.js',
