d5a7b77c4e2e2aae12f41562b0d44b527e52132a
Leon Strauss <leon@ilu.st>
2016-01-27 16:01:08 +0100

    Implemented timeouts

--- src/NavController.js
+++ src/NavController.js
@@ -1,23 +1,26 @@
-(function() {
+(function () {
 
-  angular.module('rms')
-  .controller('NavController', [
-    '$state',
-    'AuthenticationService',
-    NavController
-  ]);
+    angular.module('rms')
+        .controller('NavController', [
+            '$state',
+            'AuthenticationService',
+            'TimeoutService',
+            NavController
+        ]);
 
-  function NavController($state, AuthenticationService) {
-    var self = this;
+    function NavController($state, AuthenticationService, TimeoutService) {
+        var self = this;
 
-    self.isInState = isInState;
+        self.isInState = isInState;
 
-    self.isLoggedIn = AuthenticationService.isLoggedIn;
-    self.logout = AuthenticationService.logout;
+        self.isLoggedIn = AuthenticationService.isLoggedIn;
+        self.logout = AuthenticationService.logout;
 
-    function isInState(state) {
-      return $state.$current.name.indexOf(state) != -1;
+        self.somethingHappened = TimeoutService.somethingHappened;
+
+        function isInState(state) {
+            return $state.$current.name.indexOf(state) != -1;
+        }
     }
-  }
 
 })();
