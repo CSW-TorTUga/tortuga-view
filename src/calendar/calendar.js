274d7c77f3b215a5fa7cb6d7b298c52b15cba71f
Leon Strauss <leon@ilu.st>
2016-01-28 16:26:29 +0100

    Implemented the calendar

---
+++ src/calendar/calendar.js
@@ -0,0 +1,20 @@
+(function() {
+
+    angular.module('calendar', [
+        'ui.router',
+        'materialCalendar',
+    ]).config([
+        '$stateProvider',
+        config
+    ]);
+
+    function config($stateProvider) {
+        $stateProvider.state('calendar', {
+            url: '/calendar',
+            templateUrl: 'src/calendar/calendar.html',
+            controller: 'CalendarController',
+            controllerAs: 'calendar'
+        });
+    }
+
+})();
