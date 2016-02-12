750847aa2a27b842aab4734d8fcc6e42f27f3954
mischa <me@mhlz.de>
2016-02-12 11:46:00 +0100

    Long tokens are now working

--- src/login/LoginController.js
+++ src/login/LoginController.js
@@ -6,26 +6,35 @@
             'ErrorToasts',
             '$state',
             '$animate',
+            '$location',
             LoginController
         ]);
 
-    function LoginController(AuthenticationService, ErrorToasts, $state, $animate) {
+    function LoginController(AuthenticationService, ErrorToasts, $state, $animate, $location) {
         var self = this;
 
         self.username = '';
         self.password = '';
+        self.longToken = false;
 
         self.login = login;
+        self.isTerminal = isTerminal;
 
         //public
         function login() {
-            AuthenticationService.login(self.username, self.password)
+            AuthenticationService.login(self.username, self.password, self.longToken)
                 .then(function () {
                     $state.go('home');
                 }).catch(function (response) {
-                    if (response.status == 401)
+                    if (response.status == 401) {
                         ErrorToasts.show("Benutzername und/oder Passwort sind falsch.", 3500, false);
+                    }
                 });
+        }
+
+        // public
+        function isTerminal() {
+            return $location.host().indexOf('192.168') != -1;
         }
     }
 
