80b566630b662e0cf61c16dd2c4ff32e4fd57cab
Hannes <hannes@guedelhoefer.de>
2016-03-25 14:25:06 +0100

    added requested chart types and a date picker for special chart types need

--- src/management/statistics/StatisticsController.js
+++ src/management/statistics/StatisticsController.js
@@ -8,14 +8,17 @@
             'RoomReservation',
             'SupportMessage',
             'User',
+            'DoorAuthorisationAttempt',
             StatisticsController
         ]);
 
 
-    function StatisticsController(DeviceReservation, Device, DeviceCategory, Major, RoomReservation, SupportMessage, User) {
+    function StatisticsController(DeviceReservation, Device, DeviceCategory, Major, RoomReservation, SupportMessage, User, DoorAuthorisationAttempt) {
         var self = this;
 
         self.allDeviceReservations = DeviceReservation.query();
+
+        self.allUsages = DoorAuthorisationAttempt.query();
 
         self.allRoomReservations = RoomReservation.query();
 
@@ -24,6 +27,9 @@
         self.allMajors = Major.query();
 
         self.allDevices = Device.query();
+
+        self.selectedDate = new Date();
+
 
         self.deviceResCount = undefined;
         self.deviceNames = undefined;
@@ -34,6 +40,12 @@
         self.reservationPerMajor = undefined;
         self.majorNames = undefined;
         self.lengthLabelsArray = undefined;
+        self.usersPerGenderArray = undefined;
+        self.weekOfMonthArray = undefined;
+        self.usagePerWeekArray = undefined;
+        self.usagePerMonthArray = undefined;
+        self.usagePerMajorAndWeekArray = undefined;
+        self.usagePerMajorAndMonthArray = undefined;
 
         self.reservationCount = reservationCount;
         self.getDeviceNames = getDeviceNames;
@@ -45,7 +57,22 @@
         self.getMajorNames = getMajorNames;
         self.lengthLabels = lengthLabels;
         self.reservationsPerLength = reservationsPerLength;
+        self.usagePerWeek = usagePerWeek;
+        self.updateDateUsages = updateDateUsages;
+        self.getWeekOfMonth = getWeekOfMonth;
+        self.usagePerMonth = usagePerMonth;
 
+
+        /**
+         * To add a new statistic, you need to add an object to this array.
+         * name: Name of the statistic
+         * type: type of the statistic one of 'Bar', 'Line', 'Pie', 'Doughnut', 'Radar', 'Polar-Atea'
+         *
+         *
+         *
+         * for mor see http://jtblin.github.io/angular-chart.js/
+         * @type {*[]}
+         */
         self.graphs = [
             {
                 name: 'Reservierungen nach Gerät',
@@ -72,11 +99,11 @@
                 legend: false
             },
             {
-                name: 'Reservierungen nach Gerät und  Wochentag',
+                name: 'Reservierungen nach Gerät und Wochentag',
                 type: 'Line',
                 data: reservationCountPerDevicePerDay,
                 labels: wrapperFunction(self.weekDays),
-                serie: wrapperFunction(['Reservierungen']),
+                serie: getDeviceNames,
                 legend: true
             },
             {
@@ -102,8 +129,276 @@
                 labels: lengthLabels,
                 serie: wrapperFunction(['Reservierungen']),
                 legend: true
+            },
+            {
+                name: 'Benutzer nach Geschlecht',
+                type: 'Pie',
+                data: usersPerGender,
+                labels: wrapperFunction(['männlich', 'weiblich', 'K.A.']),
+                serie: wrapperFunction(['Benutzer']),
+                legend: true
+            },
+            {
+                name: 'Benutzung nach Woche',
+                type: 'Line',
+                data: usagePerWeek,
+                labels: getWeekOfMonth,
+                serie: wrapperFunction(['emoji', 'qr code', 'Daueröffnungen', 'Alle']),
+                legend: true,
+                needsDate: true
+            },
+            {
+                name: 'Benutzung nach Monat',
+                type: 'Line',
+                data: usagePerMonth,
+                labels: getMonth,
+                serie: wrapperFunction(['emoji', 'qr code', 'Daueröffnungen', 'Alle']),
+                legend: true,
+                needsDate: true
+            },
+            {
+                name: 'Benutzung nach Studiengang und Woche',
+                type: 'Line',
+                data: usagePerMajorAndWeek,
+                labels: getWeekOfMonth,
+                serie: getMajorNames,
+                legend: true,
+                needsDate: true
+            },
+            {
+                name: 'Benutzung nach Studiengang und Monat',
+                type: 'Line',
+                data: usagePerMajorAndMonth,
+                labels: getMonth,
+                serie: getMajorNames,
+                legend: true,
+                needsDate: true
             }
         ];
+
+        updateDateUsages();
+
+        //public
+        function updateDateUsages() {
+            updateWeekOfMonth();
+            updateUsagePerWeek();
+            updateMonth();
+            updateUsagePerMonth();
+            updateUsagePerMajorAndWeek();
+            updateUsagePerMajorAndMonth();
+        }
+
+
+
+
+        //public
+        function usagePerMajorAndMonth() {
+            return self.usagePerMajorAndMonthArray;
+        }
+
+        function updateUsagePerMajorAndMonth() {
+            self.usagePerMajorAndMonthArray = [];
+
+            self.allMajors.forEach(function(major) {
+
+                var ret = [];
+                var originalDate = angular.copy(self.selectedDate);
+
+
+                originalDate.setMonth(originalDate.getMonth() - 11);
+                for(var i = 0; i < 12; i++) {
+                    var userSet = getUsageUserPerMonthByType(originalDate);
+
+                    ret.push(0);
+                    userSet.forEach(function(userId) {
+                        var user = getUser(userId);
+                        if(user.major != undefined && user.major.id == major.id) {
+                            ret[i]++;
+                        }
+                    });
+                    originalDate.setMonth(originalDate.getMonth() + 1);
+                }
+                self.usagePerMajorAndMonthArray.push(ret);
+            });
+        }
+        
+        
+        
+        //public
+        function usagePerMajorAndWeek() {
+            return self.usagePerMajorAndWeekArray;
+        } 
+        
+        function updateUsagePerMajorAndWeek() {
+            self.usagePerMajorAndWeekArray = [];
+            
+            self.allMajors.forEach(function(major) {
+
+                var ret = [];
+                var originalDate = angular.copy(self.selectedDate);
+                originalDate.setDate(originalDate.getDate() - 10 * 7);
+                for(var i = 0; i < 10; i++) {
+                    var userSet = getUsageUserPerWeekByType(originalDate);
+
+                    ret.push(0);
+                    userSet.forEach(function(userId) {
+                        var user = getUser(userId);
+                        if(user.major != undefined && user.major.id == major.id) {
+                            ret[i]++;
+                        }
+                    });
+                    originalDate.setDate(originalDate.getDate() + 7);
+                }
+                self.usagePerMajorAndWeekArray.push(ret);
+            });
+        }
+
+        //public
+        function getWeekOfMonth() {
+            return self.weekOfMonthArray;
+        }
+
+
+        //public
+        function updateWeekOfMonth() {
+            self.weekOfMonthArray = [];
+            var startDate = angular.copy(self.selectedDate);
+            var endDate = angular.copy(self.selectedDate);
+            endDate.setDate(endDate.getDate() + 6);
+            startDate.setDate(startDate.getDate() - 10 * 7);
+            endDate.setDate(endDate.getDate() - 10 * 7);
+            for(var i = 0; i < 10; i++) {
+                self.weekOfMonthArray[i] = startDate.toDateString() + "\n- " + endDate.toDateString();
+                endDate.setDate(endDate.getDate() + 7);
+                startDate.setDate(startDate.getDate() + 7);
+            }
+
+        }
+
+        function getMonth() {
+            return self.monthArray;
+        }
+
+        function updateMonth() {
+
+            var monthNames = [
+                "Januar", "Februar", "März",
+                "April", "Mai", "Juni", "Juli",
+                "August", "September", "Oktober",
+                "November", "December"
+            ];
+
+
+            self.monthArray = [];
+            var originalDate = angular.copy(self.selectedDate);
+
+            originalDate.setMonth(originalDate.getMonth() - 11);
+            for(var i = 0; i < 12; i++) {
+                self.monthArray.push(monthNames[originalDate.getMonth()] + " " + originalDate.getFullYear());
+                originalDate.setMonth(originalDate.getMonth() + 1);
+            }
+        }
+
+
+
+        //public
+        function usagePerMonth() {
+            return self.usagePerMonthArray;
+        }
+
+
+        function getUsageUserPerMonthByType(dayInMonth, type) {
+            dayInMonth = new Date(dayInMonth);
+            dayInMonth.setDate(1);
+            var startTime = dayInMonth.valueOf();
+
+            dayInMonth.setMonth(dayInMonth.getMonth() + 1);
+            dayInMonth.setDate(0);
+            dayInMonth.setHours(23,59);
+            var endTime = dayInMonth.valueOf();
+
+            var userSet = new Set();
+            self.allUsages.forEach(function(usage) {
+                if(usage.successful && (type == undefined || usage.authType == type) && startTime < usage.time && usage.time < endTime)  {
+                    userSet.add(usage.user.id);
+                }
+            });
+            return userSet;
+
+        }
+
+        function updateUsagePerMonth() {
+            self.usagePerMonthArray = [];
+
+            var types = ["EMOJIS", "QR_CODE", "ROOM_RESERVATION", undefined];
+            types.forEach(function(type) {
+
+                var ret = [];
+                var originalDate = angular.copy(self.selectedDate);
+                originalDate.setMonth(originalDate.getMonth() - 11);
+                for(var i = 0; i < 12; i++) {
+                    ret.push(getUsageUserPerMonthByType(originalDate, type).size);
+                    originalDate.setMonth(originalDate.getMonth() + 1);
+                }
+                self.usagePerMonthArray.push(ret);
+            });
+        }
+
+        //public
+        function usagePerWeek() {
+            return self.usagePerWeekArray;
+        }
+
+
+        function getUsageUserPerWeekByType(startDay, type) {
+            var startTime = startDay.valueOf();
+            var endTime = startDay.valueOf() + 7 * 24 * 60 * 60 * 1000;
+
+            var userSet = new Set();
+            self.allUsages.forEach(function(usage) {
+                if(usage.successful && (type == undefined || usage.authType == type) && startTime < usage.time && usage.time < endTime)  {
+                    userSet.add(usage.user.id);
+                }
+            });
+            return userSet;
+
+        }
+
+        function updateUsagePerWeek() {
+            self.usagePerWeekArray = [];
+
+            var types = ["EMOJIS", "QR_CODE", "ROOM_RESERVATION", undefined];
+            types.forEach(function(type) {
+
+                var ret = [];
+                var originalDate = angular.copy(self.selectedDate);
+                originalDate.setDate(originalDate.getDate() - 10 * 7);
+                for(var i = 0; i < 10; i++) {
+                    ret.push(getUsageUserPerWeekByType(originalDate, type).size);
+                    originalDate.setDate(originalDate.getDate() + 7);
+                }
+                self.usagePerWeekArray.push(ret);
+            });
+        }
+
+
+        //public
+        function usersPerGender() {
+            if(self.usersPerGenderArray == undefined && self.allUsers.length != 0) {
+                self.usersPerGenderArray = [0,0,0];
+
+                self.allUsers.forEach(function(user) {
+                    if(user.gender == "MALE") {
+                        self.usersPerGenderArray[0]++;
+                    } else if(user.gender == "FEMALE") {
+                        self.usersPerGenderArray[1]++;
+                    } else {
+                        self.usersPerGenderArray[2]++;
+                    }
+                });
+            }
+            return self.usersPerGenderArray;
+        }
 
 
         //public
